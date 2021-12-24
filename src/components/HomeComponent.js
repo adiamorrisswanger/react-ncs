6c4e558a0e0eae9a52c2f24da3ac5f6ae22cbb1b
Adia Morris Swanger <adiamorrisswanger@gmail.com>
2021-12-23 23:35:45 -0600

    Navigation

---
+++ src/components/HomeComponent.js
@@ -0,0 +1,11 @@
+import { React } from 'react';
+
+function Home(props) {
+    return (
+        <dive className="container">
+            <h4>Home</h4>
+        </dive>
+    );
+}
+
+export default Home;
